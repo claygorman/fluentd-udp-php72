d2eaf39c5b079663389b942b8633aa3341e6daca
Clay Gorman <cgorman@theorchard.com>
2018-03-14 17:02:46 -0400

    Updating to be more robust

--- app/test-script.php
+++ app/test-script.php
@@ -5,7 +5,7 @@
  * Date: 3/13/18
  * Time: 4:25 PM
  */
-sleep(5);
+sleep(30);
 $time_start = microtime(true);
 
 require __DIR__ . '/vendor/autoload.php';
@@ -21,8 +21,20 @@ $udpHandler->setFormatter(new JsonFormatter());
 $logger->pushHandler($udpHandler);
 
 $count = 0;
-while ($count <= 10000) {
-    $logger->addInfo('Monolog test');
+while ($count <= 10) {
+    $data = [
+        'correlation_id' => '6abf812a-2386-11e8-a428-0242ac110002.1.9.1.1',
+        'environment' => 'prod',
+        'level' => 200,
+        'level_name' => 'INFO',
+        'message' => 'Correlation-Id (6abf812a-2386-11e8-a428-0242ac110002.1.9.1.1) received.',
+        'service' => 'ows-product',
+        'service_version' => '1.0.0',
+        'tag' => 'ows1',
+        'timestamp' => '2018-03-09T10:41:24.802469'
+    ];
+    $event = json_encode($data);
+    $logger->addInfo($event);
     $count++;
 }
 
